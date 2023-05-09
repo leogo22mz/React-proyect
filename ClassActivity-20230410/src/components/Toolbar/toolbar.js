f92316515106e0e6277b792ce9a9a9723db2dd37
leogo2206mz <leogo2206@gmail.com>
2023-05-09 01:22:14 +0100

    botones sin letras

--- ClassActivity-20230410/src/components/Toolbar/toolbar.js
+++ ClassActivity-20230410/src/components/Toolbar/toolbar.js
@@ -1,13 +1,68 @@
+import React, { useEffect } from "react";
 import "./toolbar.css";
+
 function Toolbar() {
+  //HREF HOME
+  useEffect(() => {
+    const buttonH = document.getElementById("buttonH");
+    buttonH.addEventListener("click", buttonHome);
+    return () => buttonH.removeEventListener("click", buttonHome);
+  }, []);
+
+  function buttonHome() {
+    window.location.href = "/";
+  }
+  //HREF EXPLORE
+  useEffect(() => {
+    const buttonE = document.getElementById("buttonE");
+    buttonE.addEventListener("click", buttonExplore);
+    return () => buttonE.removeEventListener("click", buttonExplore);
+  }, []);
+
+  function buttonExplore() {
+    window.location.href = "/explore";
+  }
+  //HREF UPLOAD
+  useEffect(() => {
+    const buttonU = document.getElementById("buttonU");
+    buttonU.addEventListener("click", buttonUpload);
+    return () => buttonU.removeEventListener("click", buttonUpload);
+  }, []);
+
+  function buttonUpload() {
+    window.location.href = "/";
+  }
+  //HREF NOTIFICATION
+  useEffect(() => {
+    const buttonN = document.getElementById("buttonN");
+    buttonN.addEventListener("click", buttonNotification);
+    return () => buttonN.removeEventListener("click", buttonNotification);
+  }, []);
+
+  function buttonNotification() {
+    window.location.href = "/center";
+  }
+  //HREF PROFILE
+  useEffect(() => {
+    const buttonP = document.getElementById("buttonP");
+    buttonP.addEventListener("click", buttonProfile);
+    return () => buttonP.removeEventListener("click", buttonProfile);
+  }, []);
+
+  function buttonProfile() {
+      <button id="buttonP" className="button-profile"><a href="/profile">P</a></button>
+    window.location.href = "/profile";
+  }
+
   return (
     <div className="tool-bar">
-
-      <button class="button-home"><a class="button-a" href="/">H</a></button>
-      <button class="button-explore"><a href="/explore">E</a></button>
-      <button class="upload-button" ><a class="upload-button-a">U</a></button>
-      <button class="button-notification"><a href="/center">C</a></button>
-      <button class="button-profile"><a href="/profile">P</a></button>
+      <button id="buttonH" className="button-home"></button>
+      <button id="buttonE" className="button-explore"></button>
+      <button id="buttonU" className="upload-button"></button>
+      <button id="buttonN" className="button-notification"></button>
+      <button id="buttonP" className="button-profile"></button>
     </div>
-  );}
-  export default Toolbar;
+  );
+}
+
+export default Toolbar;
