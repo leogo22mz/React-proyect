d6247b0c8de0607307a76684e01e5f75961283e4
leogo2206mz <leogo2206@gmail.com>
2023-05-15 00:49:48 +0100

    En este commit la implementacion de Firebase está comentada, arreglar muestreo del REALTIME DATABASE!

--- ClassActivity-20230410/src/services/recipes.service.js
+++ ClassActivity-20230410/src/services/recipes.service.js
@@ -2,7 +2,7 @@ const recipes = [
   {
     title: "Curry",
     img: "curry.jpg",
-    difficulty: 70000,
+    difficulty: "Easy",
     ingredients: ["400 g. de contramuslo de pollo",
     "½ paquete de curry japonés",
     "2 hojas de laurel (opcional)1 cebolla mediana",
@@ -26,62 +26,71 @@ const recipes = [
     " Remover bien y dejar cocinar a fuego lento durante 20 minutos y nuestro curry japonés ya está listo para disfrutar",
     " Para servir, poner un poco de arroz japonés recién cocido en un plato, el curry a un lado y opcionalmente para decorar poner un poco de alga Nori en polvo por encima",
 ]},{
-    difficulty: 70000,
-    recipe: "A45",
-    title: "Curry",
-    ingredients: ["400 g. de contramuslo de pollo",
-    "½ paquete de curry japonés","2 hojas de laurel (opcional)1 cebolla mediana"," 2 patatas medianas (unos 400 g.), 2 zanahorias ( unos 250 g.), 2 dientes de ajo 4 rodajitas de jengibre, ½ cucharadita de sal, ½ cucharadita de pimienta negra, Agua (hasta que cubra el pollo), Aceite vegetal",
-  ],
-    img: "fridge.png"
-  },{
-    difficulty: 70000,
-    recipe: "A45",
-    title: "Curry",
-    ingredients: ["400 g. de contramuslo de pollo",
-    "½ paquete de curry japonés","2 hojas de laurel (opcional)1 cebolla mediana"," 2 patatas medianas (unos 400 g.), 2 zanahorias ( unos 250 g.), 2 dientes de ajo 4 rodajitas de jengibre, ½ cucharadita de sal, ½ cucharadita de pimienta negra, Agua (hasta que cubra el pollo), Aceite vegetal",
-  ],
-    img: "fridge.png"
-  },{
-    difficulty: 70000,
-    recipe: "A45",
-    title: "Curry",
-    ingredients: ["400 g. de contramuslo de pollo",
-    "½ paquete de curry japonés","2 hojas de laurel (opcional)1 cebolla mediana"," 2 patatas medianas (unos 400 g.), 2 zanahorias ( unos 250 g.), 2 dientes de ajo 4 rodajitas de jengibre, ½ cucharadita de sal, ½ cucharadita de pimienta negra, Agua (hasta que cubra el pollo), Aceite vegetal",
-  ],
-    img: "fridge.png"
-  },{
-    difficulty: 70000,
-    recipe: "A45",
-    title: "Curry",
-    ingredients: ["400 g. de contramuslo de pollo",
-    "½ paquete de curry japonés","2 hojas de laurel (opcional)1 cebolla mediana"," 2 patatas medianas (unos 400 g.), 2 zanahorias ( unos 250 g.), 2 dientes de ajo 4 rodajitas de jengibre, ½ cucharadita de sal, ½ cucharadita de pimienta negra, Agua (hasta que cubra el pollo), Aceite vegetal",
-  ],
-    img: "fridge.png"
-  },{
-    difficulty: 70000,
-    recipe: "A45",
-    title: "Curry",
-    ingredients: ["400 g. de contramuslo de pollo",
-    "½ paquete de curry japonés","2 hojas de laurel (opcional)1 cebolla mediana"," 2 patatas medianas (unos 400 g.), 2 zanahorias ( unos 250 g.), 2 dientes de ajo 4 rodajitas de jengibre, ½ cucharadita de sal, ½ cucharadita de pimienta negra, Agua (hasta que cubra el pollo), Aceite vegetal",
-  ],
-    img: "fridge.png"
-  },{
-    difficulty: 70000,
-    recipe: "A45",
-    title: "Curry",
-    ingredients: ["400 g. de contramuslo de pollo",
-    "½ paquete de curry japonés","2 hojas de laurel (opcional)1 cebolla mediana"," 2 patatas medianas (unos 400 g.), 2 zanahorias ( unos 250 g.), 2 dientes de ajo 4 rodajitas de jengibre, ½ cucharadita de sal, ½ cucharadita de pimienta negra, Agua (hasta que cubra el pollo), Aceite vegetal",
-  ],
-    img: "fridge.png"
-  },{
-    difficulty: 70000,
-    recipe: "A45",
-    title: "Curry",
-    ingredients: ["400 g. de contramuslo de pollo",
-    "½ paquete de curry japonés","2 hojas de laurel (opcional)1 cebolla mediana"," 2 patatas medianas (unos 400 g.), 2 zanahorias ( unos 250 g.), 2 dientes de ajo 4 rodajitas de jengibre, ½ cucharadita de sal, ½ cucharadita de pimienta negra, Agua (hasta que cubra el pollo), Aceite vegetal",
-  ],
-    img: "fridge.png"
-  },
+  title: "Tomato Chicken Wings",
+  img: "wings.jpg",
+  difficulty: "Easy",
+  ingredients: ["Medio kilo de Alitas de pollo",
+  "200ml de Salsa de tomate",
+  "Especias al gusto",
+  " Hierbas provenzales al gusto",
+  " 2 zanahorias ( unos 250 g.)",
+  " 2 dientes de ajo 4 rodajitas de jengibre",
+  " ½ cucharadita de sal"," ½ cucharadita de pimienta negra",
+  " Agua (hasta que cubra el pollo)",
+  " Aceite vegetal"],
+  steps: [
+  "Cortamos las alitas separando las dos porciones.",
+  " Utilizaremos dos bolsas que cerraremos al vacío, metiendo en una los muslitos y en la otra las otras secciones.",
+  " En cada bolsa, añadimos una cucharada de aceite de oliva, y las hierbas y especias que más nos gusten para condimentar nuestras alitas",
+  " Cocinar las alitas dentro de las bolsas, con el fuego al mínimo, evitando que el agua llegue a hervir. Con unos 40 minutos será suficiente para dejar las alitas bien cocinadas por dentro y tiernísimas.",
+  " Finalizamos la receta friendo las alitas y escurriéndolas bien en un papel absorbente.",
+  " Después les faltarán nada más unos minutos de cocción dentro de una cacerola, cubiertas con el tomate frito.",
+]},{
+  title: "Cachopo",
+  img: "cachopo.jpg",
+  difficulty: "Medium High",
+  ingredients: ["4 filetes de ternera cortados dobles, tipo librillo para San Jacobo (unos 120 g. la unidad)",
+  "4 lonchas de jamón serrano (se puede sustituir por cecina)",
+  "8 lonchas de queso (Queso de Oscos, cheddar, gruyere, Edam,... y si os gusta su sabor 4 de queso suave y lascas de Cabrales)",
+  " Sal (al gusto) ",
+  " 100 g. de harina de trigo  ",
+  " 2 huevos grandes y 200 g. de pan rallado  ",
+  " 4 patatas gallegas grandes (Para acompañar)",
+  " Aceite de oliva virgen extra suave (el necesario para freír)"],
+  steps: [
+  "Preparamos nuestros ingredientes para realizar la receta de manera organizada y más rápida. En 3 platos disponemos la harina, el pan rallado y los 2 huevos batidos. Reservamos hasta el momento del rebozado.",
+  " Usaremos un par de filetes por cada cachopo que sean aproximadamente del mismo tamaño y forma, para que casen perfectamente el uno sobre el otro.",
+  " Los filetes de cadera son muy tiernos y perfectos para esta receta. Los limpiaremos bien de las telillas de los bordes para que no se nos «encojan» al freírlos. Los vamos a trabajar con un mazo o una piedra tal como os comentaba antes, para ablandarlos y aplanarlos.",
+  " Extendemos un filete y salamos al gusto. En una de las mitades colocamos  una loncha de jamón serrano (o cecina, el sabor es increíble con ella) y sobre ella una de queso (recordad que podemos poner varios tipos de queso, el que más os guste).",
+  " Cerramos con la otra mitad del filete apretando en los bordes. Así conseguiremos que se una bien la carne y quede el relleno en el interior. Hacemos lo mismo con los filetes restantes.",
+  " Los empanaremos con el clásico rebozado, pasándolos por harina, huevo y pan rallado.",
+  " Lo mejor es rebozar con una sola capa para que no sea demasiado grueso, así resultan más ligeros y absorben menos aceite en la fritura.",
+  " Como acompañamiento en la mayoría de los restaurantes asturianos podéis elegir entre muchos tipos de salsa. Por ejemplo, una salsa de Cabrales, salsa de sidra, cebolla caramelizada, salsa de tomate natural a la pimienta, verduras a la plancha o unos pimientos caramelizados al ajillo.",
+]},{
+  title: "Scrambled Eggs",
+  img: "eggs.jpeg",
+  difficulty: "Easy",
+  ingredients: ["5 huevos camperos",
+  "1 cucharada colmada de aceite de oliva virgen extra o de mantequilla",
+  "Sal y pimienta negra recién molida (al gusto de cada casa)",
+  " Opcional: Cebollino picado o un poquito de perejil fresco",],
+  steps: [
+  "En una sartén en la que no se peguen los ingredientes (lo mejor es esa que tenéis para la tortilla, para unos crepes o para la tortilla francesa) vamos a preparar unos huevos revueltos simples y rápidos de hacer.",
+  " Echamos los huevos a la sartén caliente y removemos constantemente y de forma envolvente con una cuchara de madera.",
+  " Debemos separar constantemente lo que se pega a las paredes y al fondo de la sartén. Cuando los huevos estén todavía húmedos, pero no totalmente líquidos, los juntamos en el centro.",
+  " Vamos a atravesar los huevos desde los bordes hacia el centro, dejando unos segundos entre pasada y pasada para darle tiempo a que vayan cuajando.",
+  " Apagamos el fuego y veremos el huevo que se sigue cocinado porque la sartén está aún caliente, retiramos a un plato. Salpimentamos y añadimos un poquito de cebollino finamente picado (de manera opcional).",
+]},{
+  title: "Banana Smoothie",
+  img: "banana.jpg",
+  difficulty: "Super Easy",
+  ingredients: ["2 plátanos maduros",
+  "300 ml. de leche semidesnatada",
+  "1-2 cucharaditas de canela"],
+  steps: [
+  "Troceamos el plátano, lo metemos en un recipiente y lo congelamos unas dos horas o toda la noche.",
+  " Ponemos el plátano, la leche y la canela en el vaso de la batidora y batimos hasta que no queden grumos.",
+  " Echamos el smoothie o batido de plátano en un vaso. Podemos espolvorear con algo más de canela."]},
 ];
 
 const getRecipes = () => {
@@ -91,3 +100,22 @@ const getRecipes = () => {
 export default {
   getRecipes
 };
+
+
+//FIREBASE (WIP)
+// export default {
+//   getRecipes
+// };
+
+// import db from "../firebase";
+// import { ref, get, remove, push } from "firebase/database";
+
+// const recipes = ref(db, "/recipes");
+
+// const getRecipes = () => {
+//   return get(recipes);
+// };
+
+// export default {
+//   getRecipes
+// };
