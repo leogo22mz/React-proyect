4842c67d477beaecc0ea799ff82312fdad977c54
leogo2206mz <leogo2206@gmail.com>
2023-05-22 00:21:50 +0100

    Rama ideal

--- ClassActivity-20230410/src/components/recipeList/RecipeList.js
+++ ClassActivity-20230410/src/components/recipeList/RecipeList.js
@@ -1,120 +1,21 @@
-// import { useState, useEffect } from "react";
-// import RecipeService from "../../services/recipes.service";
-// import "./RecipeList.css";
-
-// function RecipeList() {
-//   const [recipes, setRecipes] = useState([]);
-//   const [query, setQuery] = useState("");
-//   const [showDescription, setShowDescription] = useState(false); // nuevo estado
-
-//   const getRecipes = () => {
-//     const allRecipes = RecipeService.getRecipes();
-//     setRecipes(allRecipes);
-//   };
-
-//   const showRecipes = () => {
-//     return (
-//       recipes
-//         .filter(
-//           (c) =>
-//             c.title.toLowerCase().includes(query.toLowerCase()) ||
-//             c.difficulty.toString().toLowerCase().includes(query.toLowerCase()) ||
-//             c.recipe.toLowerCase().includes(query.toLowerCase())
-//         )
-//         .map((c) => {
-//           return (
-//             <div className="car-list-item">
-//               <br/><br/>
-//               <h1>{c.title}</h1>
-//               <p>{c.difficulty}</p>
-//               <div className="car-list-img">
-//                 <img src={`/assets/img/${c.img}`} alt="car" />
-//               </div>
-//               <div
-//                 className="car-list-description"
-//                 style={{ display: showDescription ? "block" : "none" }}
-//               >
-//                 <br />
-//                 <h3>INGREDIENTS:</h3>
-//                 <ul>
-//                   {c.ingredients &&
-//                     c.ingredients.map((ingredient) => {
-//                       return <li key={ingredient}>{ingredient}</li>;
-//                     })}
-//                 </ul>
-//                 <br />
-//                 <h3>STEPS:</h3>
-//                 {Array.isArray(c.steps) ? (
-//                   <ol>
-//                     {c.steps.map((step) => {
-//                       return <li key={step}>{step}</li>;
-//                     })}
-//                   </ol>
-//                 ) : (
-//                   <p>No steps found.</p>
-//                 )}
-//               </div>
-//               <br />
-//               <button
-//                 className="read-more-btn"
-//                 onClick={() => setShowDescription(!showDescription)}
-//               >
-//                 {showDescription ? "Read Less" : "Read More"} 
-//               </button>
-//             </div>
-//           );
-//         })
-//     );
-//   };
-
-//   const handleChange = (event) => {
-//     const searchValue = event.target.value;
-//     setQuery(searchValue);
-//   };
-
-//   useEffect(() => {
-//     getRecipes();
-//   }, []);
-
-//   return (
-//     <>
-//       {/* ACTUALIZAR PARA EL SEARCH */}
-//       {/* <input type="search" onChange={handleChange} /> */}
-//       <div className="main-container">
-//         <div className="car-list-container">{showRecipes()}</div>
-//         <div className="right-text"></div>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default RecipeList;
-
-
-//FIREBASE (WIP)
-
-
 import "./RecipeList.css";
 import { useState, useEffect } from "react";
 import RecipeService from "../../services/recipes.service";
 
-
 function RecipeList() {
   const [recipes, setRecipes] = useState([]);
   const [query, setQuery] = useState("");
-  const [showDescription, setShowDescription] = useState(false); // nuevo estado
-
+  const [showDescription, setShowDescription] = useState({});
 
   const getRecipes = () => {
     RecipeService.getRecipes()
       .then((items) => {
-        console.log("hola")
         let allRecipes = [];
-        items.forEach(item => {
+        items.forEach((item) => {
           const key = item.key;
           const data = item.val();
           allRecipes.push({
-            key: key,
+            id: key, // Utilizamos el campo "key" como identificador único
             title: data.title,
             img: data.img,
             difficulty: data.difficulty,
@@ -125,13 +26,9 @@ function RecipeList() {
         setRecipes(allRecipes);
       })
       .catch((err) => {
-        console.log("errrorecito")
         console.error(err);
       });
-    console.log("adiós")
-  }
-
-
+  };
 
   const showRecipes = () => {
     return (
@@ -144,16 +41,14 @@ function RecipeList() {
         )
         .map((c) => {
           return (
-            <div className="car-list-item" key={c.key}>
+            <div className="car-list-item" key={c.id}>
+              <br/><br/>
               <h1>{c.title}</h1>
-              <p>{c.difficulty}</p>
+              <p>Difficulty: {c.difficulty}</p>
               <div className="car-list-img">
                 <img src={`/assets/img/${c.img}`} alt="car" />
               </div>
-              <div
-                className="car-list-description"
-                style={{ display: showDescription ? "block" : "none" }}
-              >
+              <div className="car-list-description" style={{ display: showDescription[c.id] ? "block" : "none" }}>
                 <br />
                 <h3>INGREDIENTS:</h3>
                 <ul>
@@ -177,16 +72,20 @@ function RecipeList() {
               <br />
               <button
                 className="read-more-btn"
-                onClick={() => setShowDescription(!showDescription)}
+                onClick={() =>
+                  setShowDescription((prevState) => ({
+                    ...prevState,
+                    [c.id]: !prevState[c.id],
+                  }))
+                }
               >
-                {showDescription ? "Read Less" : "Read More"}
+                {showDescription[c.id] ? "Read Less" : "Read More"}
               </button>
             </div>
           );
         })
     );
   };
-
 
   const handleChange = (event) => {
     const searchValue = event.target.value;
